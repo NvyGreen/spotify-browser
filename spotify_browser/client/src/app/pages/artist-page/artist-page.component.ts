95f746b4827f15c4fd16356899840fd25cbb8188
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-02-26 20:32:22 -0800

    Add files via upload

---
+++ spotify_browser/client/src/app/pages/artist-page/artist-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArtistData } from '../../data/artist-data';
+import { TrackData } from '../../data/track-data';
+import { AlbumData } from '../../data/album-data';
+import { SpotifyService } from 'src/app/services/spotify.service';
+
+@Component({
+    selector: 'app-artist-page',
+    templateUrl: './artist-page.component.html',
+    styleUrls: ['./artist-page.component.css'],
+    standalone: false
+})
+export class ArtistPageComponent implements OnInit {
+	artistId:string;
+	artist:ArtistData;
+	topTracks:TrackData[];
+	albums:AlbumData[];
+
+  constructor(private route: ActivatedRoute, private spotifyService: SpotifyService) { }
+
+  ngOnInit() {
+    this.artistId = this.route.snapshot.paramMap.get('id');
+    //TODO: Inject the spotifyService and use it to get the artist data, top tracks for the artist, and the artist's albums
+
+    this.spotifyService.getArtist(this.artistId).then((response) => {
+      this.artist = response;
+    }, (err) => {
+      console.error(err);
+    });
+
+    this.spotifyService.getTopTracksForArtist(this.artistId).then((response) => {
+      this.topTracks = response;
+    }, (err) => {
+      console.error(err);
+    });
+
+    this.spotifyService.getAlbumsForArtist(this.artistId).then((response) => {
+      this.albums = response;
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+}
---
+++ spotify_browser/readme.txt
@@ -0,0 +1,33 @@
+--Readme document for Nivedha Aravindmali, naravind@uci.edu--
+
+1. How many assignment points do you believe you completed (replace the *'s with your numbers)?
+
+*/10
+- 1/1 Communicating with the webserver
+- 1/1 Populating information about the user
+- 3/3 Populating the search component
+- 3/3 Artist page
+- 1.5/1.5 Album page
+- 0.5/0.5 Track page
+
+
+2. How long, in hours, did it take you to complete this assignment?
+It took me about 20 hours to complete this assignment.
+
+
+3. What online resources did you consult when completing this assignment? (list specific URLs)
+I looked at the Spotify API documentation (https://developer.spotify.com/documentation/web-api), Angular documentation 
+for the @for (https://angular.dev/api/core/@for) and @if (https://angular.dev/api/core/@if) loops, and did some general 
+Googling if I didn't know how to program something.
+
+
+4. What classmates or other individuals did you consult as part of this assignment? What did you discuss?
+I did not consult anyone else on this assignment, as I worked alone.
+
+
+5. Did you add a bonus feature to your submission? If so, what is it and how should we see it?
+I did not add a bonus feature to my assignment.
+
+
+6. Is there anything special we need to know in order to run your code?
+Nothing different from my previous 2 submissions.
